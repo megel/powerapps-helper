4c855f8a511a5a123b17fb26395a1f15ee8e0690
Michael Megel <michael.megel@cosmoconsult.com>
2021-03-12 00:01:03 +0100

    test

--- src/test/suite/languageTool.test.ts
+++ src/test/suite/languageTool.test.ts
@@ -1,17 +1,25 @@
 import * as assert from 'assert';
 import Sinon = require('sinon');
 import * as vscode from 'vscode';
+import * as myExtension from '../../extension';
+import {Utils} from '../../helpers/Utils';
 
 suite('Source File Utility Test', () => {
+
+    test('checkSourceFileUtilityFolder', async () => {
+        const fs = require('fs');
+        const binPath = await Utils.getSourceFileUtility();
+        assert.strictEqual(fs.existsSync(binPath), true, "File was found");
+    }).timeout(10000);
 
     test('checkSourceFileUtility', async () => {
         let infoMessages = Sinon.stub(vscode.window, "showInformationMessage");
         let errorMessages = Sinon.stub(vscode.window, "showErrorMessage");
         await vscode.commands.executeCommand('mme2k-powerapps-helper.checkSourceFileUtility');
 
-        assert.strictEqual(errorMessages.callCount, 0);
-        assert.strictEqual(infoMessages.callCount, 1);
-        assert(infoMessages.firstCall.calledWith(Sinon.match("was found")));
+        assert.strictEqual(errorMessages.callCount, 0, `Error was shown ... ${errorMessages.callCount}`);
+        assert.strictEqual(infoMessages.callCount, 1, `No Info was shown ... ${infoMessages.callCount}`);
+        assert(infoMessages.firstCall.calledWith(Sinon.match("was found")), "Language Tool was not found");
     }).timeout(10000);
 
 });
